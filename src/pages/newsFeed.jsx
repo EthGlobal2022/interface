413a90882bdc01e73dc16c08acb255a0679b94f1
Harsh Ghodkar <harshghodkar@gmail.com>
2022-02-07 02:18:40 +0530

    updates

--- src/pages/newsFeed.jsx
+++ src/pages/newsFeed.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from "react";
 import UserCard from "./card";
 import styles from "styles/cards.module.css";
